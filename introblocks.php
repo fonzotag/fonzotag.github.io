8cd52cdac04256c975ec07488ae01d701a13ba06
fonzotag <fonzotag@gmail.com>
2017-07-14 09:28:38 -0500

    Add files via upload

---
+++ introblocks.php
@@ -0,0 +1,25 @@
+<?php
+$portal='<p class="header"><font size="5px">Where do I start?</font></p>
+<p class="body">Look above at the navigation portal, simply select the portal that applies most closely to your position within the district as the content is specific to what you should need.</p><br>';
+$links='<p class="header"><font size="5px">How do I find the website I came here for?</font></p>
+<p class="body">Look to the left at the navigation menu, if you are unsure of exactly which site you\'re looking for just hold your mouse over the links for a short description! Be aware that this menu will change depending on which portal you select.</p><br>';
+$guidebar='<p class="header"><font size="5px">Where is some additional information?</font></p>
+<p class="body">If you look toward the bottom of this page you should see our guide bar which contains all of the document and video guides we have available. Please excuse our progress as we are constantly revising and adding to this section!</p><br><br>';
+$needmore='<p class="header"><font size="5px">Not finding what you\'re looking for?</font></p>
+<p>We are sorry you were unable to locate the information you came here for! We would love to hear your feedback. If there is a link or some information you feel we should add please email us at Helpdesk@apsb.org with the subject line of "New Helpdesk Website" and let us know!</p>';
+?>
+
+
+
+
+
+
+
+
+<style>
+.header {
+font-style: oblique;
+}
+.body {
+}
+</style>
