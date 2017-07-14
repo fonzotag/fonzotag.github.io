407c1a565cd40813e1ff4b06a68bcc296131888c
fonzotag <fonzotag@gmail.com>
2017-07-14 09:11:41 -0500

    Add files via upload

---
+++ description.php
@@ -0,0 +1,30 @@
+<?php
+$blackboard='Use to deliver curriculum material or collaborate with parents, teachers, and students';
+$compass='Compass';
+$datawarehouse='Restricted Access';
+$destiny='Library and Asset Catalog';
+$drcinsight='State Testing Platform';
+$employeeportal='Login to view your personal payment information, including check stubs and vacation time';
+$exchange='View your old APSB email archive here';
+$gmail='Access your APSB email here';
+$illuminatestudent='IlluminateHC; take assigned tests and assessments here<br>(949) 242-0343';
+$illuminateteacher='IlluminateEd; create/administer tests and assessments here<br>(949) 242-0343';
+$kboxschools='Enter your technology work orders here';
+$kellyeducationalstaffing='Teachers, enter your absences here';
+$novatime='Faculty, enter your absences here<br>(877) 486-6682';
+$office365='Office365';
+$onlineschoolpayments='Known as OSP; use to make payments for any fees collected by your school including lunch, supplies, and enrollment<br>(877) 378-8290';
+$onlineteacherreceipting='Known as OTR; use to print record receipts for any payment not made through OSP<br>(877) 378-8290';
+$passwordreset='Passwords expire every 90 days, reset your password here';
+$powerschooladmin='Restricted Access';
+$powerschoolstudent='Login with student information; use to view grades, attendance, and lunch balances';
+$powerschoolteacher='Use to enter student attendance and grades';
+$registerme='Create, register for, and manage upcoming events here';
+$safeschools='Complete required safety trainings here by logging with your employee ID';
+$schooldude='Enter maintenance work orders here';
+$sharepoint='Sharepoint';
+$springboard='Use to complete assigned curriculum workbooks<br>(877) 999-7723';
+$tedk12hire='Apply for a job with Ascension Public Schools here (2016-2017)';
+$applicantportal='Apply for a job with Ascension Public Schools here (2017-*)';
+$googleguides='A portal to all of our comprehensive guides regarding Google Suite';
+?>
---
+++ guidebar.php
@@ -0,0 +1,100 @@
+<link rel="stylesheet" href="css/guidebar.css" type="text/css" />
+<div id="guidebar">
+    <ul>
+<!--    <div class="dropdown">
+   		<li>DRC Eagle</li>
+        <div class="dropdown-content">
+        <li class="disabled">Documents</li>
+        <li><a href="https://drive.google.com/file/d/0B_AXn_3cCzgeVVZjc2kzTUFIdGM/view?usp=sharing" title="How To Access: Teachers" target="_blank" onclick="leaveAlert()">How To Access</a></li>
+        <li><a href="https://drive.google.com/file/d/0B_AXn_3cCzgeaVNTcDJYbGFGRUE/view?usp=sharing" title="Complete Teacher User Guide" target="_blank" onclick="leaveAlert()">User Guide</a></li>
+        <li><a href="https://drive.google.com/file/d/0B_AXn_3cCzgeT2pvMkFHZkJ5VzA/view?usp=sharing" title="Adding A Student" target="_blank" onclick="leaveAlert()">Add A Student</a></li>
+        <li><a href="https://drive.google.com/file/d/0B_AXn_3cCzgeNDlDLXRCVnNJQjg/view?usp=sharing" title="Adding Students To Groups: Test Session" target="_blank" onclick="leaveAlert()">Add To Session</a></li>
+        <li><a href="https://drive.google.com/file/d/0B_AXn_3cCzgeZGxQeVVPWUgtbGs/view?usp=sharing" title="Configuration Codes" target="_blank" onclick="leaveAlert()">Configuration Codes</a></li>
+       <li><a href="guides/drcchromeott/LEAP__DRC_Insight_Testing_-_ChromeBooks_Online_Tools_Training.html" title="LEAP DRC Insight Testing - Chromebooks Online Tools Training">Chromebook Tools</a></li>
+         <li><a href="guides/drcnoteott/EOC_DRC_Insight_Testing_-_Notebooks_Online_Tools_Training.html" title="EOC DRC Insight testing - Notebooks Online Tools Training">Notebook Tools</a></li>
+         <li><a href="https://drive.google.com/file/d/0B_AXn_3cCzgedVJrM21ZUjR5Zmc/view?usp=sharing" title="How To Create A Test" target="_blank" onclick="leaveAlert()">Creating Tests</a></li>
+         <li><a href="https://drive.google.com/file/d/0B_AXn_3cCzgeeGZvRzVIZ2tQODQ/view?usp=sharing" title="How To Create Test Sessions" target="_blank" onclick="leaveAlert()">Creating Session</a></li>
+         <li><a href="https://drive.google.com/file/d/0B_AXn_3cCzgeMW5QUVNDRDl6eDA/view?usp=sharing" title="Generating Test Session Tickets" target="_blank" onclick="leaveAlert()">Generating Tickets</a></li>
+         <li><a href="https://drive.google.com/file/d/0B_AXn_3cCzgeTmJ0ZUhMRjgwSkE/view?usp=sharing" title="Printing Test Session Tickets" target="_blank" onclick="leaveAlert()">Printing Tickets</a></li>
+         <li><a href="guides/drccblogin/EAGLE__DRC_Insight_Testing_-_ChromeBooks.html" title="How Students Login: ChromeBooks">Chromebook Login</a></li>
+         <li><a href="guides/drccblogin/EAGLE__DRC_Insight_Testing_-_ChromeBooks.html" title="How To Students Login - Notebooks">Notebook Login</a></li>
+      </div></div> -->
+       
+       <div class="dropdown">
+       <li tabindex="1">Backup</li>
+       <div class="dropdown-content">
+		<li class="disabled">Documents</li>
+        <li><a href="guides/restorefiles/How_To_Request_Restoration_Of_Missing_Files.html" title="How To Request Restoration Of Missing Files" tabindex="1">Request Lost Files</a></li>
+        <li><a href="guides/backupyourfileswindows/How_To_Backup_Your_Files_-_Windows.html" title="How To Backup Your Files - Windows" tabindex="1">Windows Backup</a></li>
+        <li><a href="guides/retrievebackup/How_To_Retrieve_Your_Backed_Up_Files.html" title="How To Retrieve Your Backed Up Files - Windows" tabindex="1">Retrieve Backup</a></li>
+       </div></div>
+       
+       <div class="dropdown">
+       <li tabindex="1">Reset Password</li>
+       <div class="dropdown-content">
+        <li class="disabled">Documents</li>
+        <li><a href="guides/resetad/How_To_Reset_PasswordEnroll_In_AD.html" title="How To Reset Password/Enroll In AD" tabindex="1">Active Directory</a></li>
+        <li><a href="guides/resetnovatime/How_To_Reset_Password_In_NovaTime.html" title="How To Reset Password In NovaTime" tabindex="1">NovaTime</a></li>
+        <li><a href="#" title="" tabindex="1">TedK12</a></li>
+        <li><a href="guides/resetpowerschool/How_To_Reset_Your_Password_In_PowerSchool.html" title="How To Reset Your Pasword In PowerSchool" tabindex="1">PowerSchool</a></li>
+        </div></div> 
+
+       <div class="dropdown">
+        <li tabindex="1">Map Drives</li>
+        <div class="dropdown-content">
+         <li class="disabled" tabindex="1">Documents</li>
+         <li><a href="guides/mapdrivemac/How_To_Map_Drive_-_Macintosh.html" title="How To Map Drive - Macintosh" tabindex="1">Macintosh</a></li>
+         <li><a href="guides/mapdrivewindows/How_To_Map_Drive_-_Windows.html" title="How To Map Drive - Windows" tabindex="1">Windows</a></li>
+         </div></div>
+                
+       <div class="dropdown">
+         <li tabindex="1">Phone</li>
+         <div class="dropdown-content">
+          <li class="disabled">Documents</li>
+          <li><a href="https://drive.google.com/file/d/0B1mSk1WwrsjTTzM2Z2pIaW1YcjA/view?usp=sharing" title="How To Change Voicemail On Your Phone" target="_blank" onclick="leaveAlert()" tabindex="1">Set Voicemail</a></li>
+          <li><a href="https://drive.google.com/file/d/0B1mSk1WwrsjTSDNIenNsaE41R1U/view?usp=sharing" title="How to Change The Voicemail Pin On Your Phone" target="_blank" onclick="leaveAlert()" tabindex="1">Set/Change Pin</a></li>
+          <li><a href="#" title="" target="_blank" onclick="leaveAlert()" tabindex="1">Request Change</a></li>
+         </div></div>
+                     
+       <div class="dropdown">
+         <li tabindex="1">See Videos</li>
+		 <div class="dropdown-content">
+     	 <li class="disabled">Documents</li>
+         <li><a href="guides/installsecuritycert/How_To_Install_The_Security_Certificate.html" title="How To Install The Security Certificate" tabindex="1">Install Certificate</a></li>
+         <li><a href="guides/requestvideoaccess/How_To_Request_Video_Access.html" title="How To Request Video Access" tabindex="1">Request Access</a></li>
+         <li><a href="guides/allowyoutubevideos/How_To_Allow_Youtube_Videos.html" title="How To Allow Youtube Videos" tabindex="1">Approve Youtube</a></li>
+         </div></div>
+                     
+       <div class="dropdown">
+         <li tabindex="1">KBOX</li>
+         <div class="dropdown-content">
+         <li class="disabled">Documents</li>
+         <li><a href="guides/enterworkorderkbox/How_To_Enter_A_Work_Order_In_KBOX.html" title="How to Enter A Work Order In KBOX" tabindex="1">Enter Work Order</a></li>
+       	 <li><a href="guides/editworkorderkbox/How_To_Edit_A_Work_Order_In_KBOX.html" title="How To Edit A Work Order In KBOX" tabindex="1">Edit Work Order</a></li>
+         <li><a href="guides/deleteworkorderkbox/How_To_Delete_A_Work_Order_In_KBOX.html" title="How To Delete A Work Order In KBOX" tabindex="1">Delete Work Order</a></li>
+         </div></div>
+                     
+       <div class="dropdown">
+         <li tabindex="1">Log In</li>
+         <div class="dropdown-content">
+       	 <li class="disabled">Documents</li>
+         <li><a href="guides/loginemployeeportal/How_To_Login_To_Employee_Portal.html" title="How To Login To Employee Portal" tabindex="1">Employee Portal</a></li>
+         <li><a href="guides/logindatawarehouse/How_To_Login_To_Data_Warehouse.html" title="How To Login To Data Warehouse" tabindex="1">Data Warehouse</a></li>
+         <li><a href="guides/loginkbox/How_To_Login_To_KBOX.html" title="How To Login To KBOX" tabindex="1">KBOX</a></li>
+         <li><a href="guides/loginemail/How_To_Login_To_Email.html" title="How To Login To Email" tabindex="1">Email</a></li>
+         <li><a href="guides/loginoldemail/How_To_Login_To_Old_Email.html" title="How To Login To Old Email" tabindex="1">Old Email</a></li>
+         <li><a href="guides/loginnovatime/How_To_Login_To_NovaTime.html" title="How To Login To NovaTime" tabindex="1">Nova Time</a></li>
+         <li><a href="guides/loginpowerschool/How_To_Login_To_PowerSchool.html" title="How To Login To PowerSchool" tabindex="1">Power School</a></li>
+         <li><a href="guides/loginsafeschools/How_To_Login_To_SafeSchools_Training.html" title="How To Login To SafeSchools Training" tabindex="1">SafeSchools</a></li>
+         <li><a href="guides/loginyoutube/How_To_Login_To_Youtube.html" title="How To Login To Youtube" tabindex="1">Youtube</a></li>
+         <li><a href="guides/loginblackboard/How_To_Login_To_BlackBoard.html" title="How To Login To BlackBoard" tabindex="1">BlackBoard</a></li>
+         <li><a href="guides/loginilluminate/How_To_Login_To_Illuminate.html" title="How To Login To Illuminate" tabindex="1">Illuminate</a></li>
+         <li><a href="guides/logintedk12/How_To_Login_To_TedK12.html" title="How To Login To TedK12" tabindex="1">TedK12</a></li>
+         <li><a href="guides/logindrc/How_To_Login_To_DRC_Insight.html" title="How To Login To DRC Insight" tabindex="1">DRC Insight</a></li>
+         <li><a href="guides/loginkellyedustaffing/How_To_Login_To_Kelly_Educational_Staffing.html" title="How To Login To Kelly Education Staffing" tabindex="1">Kelly Ed Staffing</a></li>
+         <li><a href="guides/loginspringboard/How_To_Login_To_Springboard.html" title="How To Login To Springboard" tabindex="1">Springboard</a></li>
+         <li><a href="guides/loginschooldude/How_To_Login_To_Schooldude.html" title="How To Login To Schooldude" tabindex="1">Schooldude</a></li>
+         </div></div>
+	</ul></div>
+    
+   
